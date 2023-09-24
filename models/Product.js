e4830eb7a3999304293b5780eb5b8872d0dd5326
CGrace <c.grace5@yahoo.com.au>
2023-09-25 00:34:23 +1000

    code complete, readme updated

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,46 @@ class Product extends Model { }
 // set up fields and rules for Product model
 Product.init(
   {
-
     // define columns
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
+    product_name: {
+      // String
+      type: DataTypes.STRING,
+      // Doesn't allow null values
+      allowNull: false,
+    },
+    price: {
+      // Decimal
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,29 +1,76 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  res.send("Hellow World");
+  Category.findAll({
+    include: {
+      model: Product
+    }
+  }).then((categories) => {
+    res.json(categories);
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Product.findAll({
+    where: {
+      category_id: req.params.id
+    }
+  }).then((products) => {
+    res.json(products);
+  });
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+
+  const create = Object.keys(req.body).length > 0 ? req.body : req.query;
+
+  if (!create.category_name) {
+    res.json({ status: "error", message: "Category is expected" });
+    return;
+  }
+
+  if (create.id) {
+    Category.findOne({
+      where: {
+        id: create.id
+      }
+    }).then((cat) => {
+      if (cat) {
+        res.json({ status: "error", message: "Category already exists" });
+        return;
+      }
+    });
+  }
+  else {
+    Category.create(create);
+    res.json({ status: "success" });
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  const update = Object.keys(req.body).length > 0 ? req.body : req.query;
+
+  if (update.id) {
+    res.json({ status: "error", message: "id is not updated" });
+    return;
+  }
+
+  Category.update(update, {
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json({ status: 'success' });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json({ status: 'success' });
 });
 
 module.exports = router;
